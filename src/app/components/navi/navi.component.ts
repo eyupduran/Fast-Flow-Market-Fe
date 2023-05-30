a81e4731666e035608018c68b29cafecbabff171
eyupveriyaz <eyup@veriyaz.com.tr>
2023-05-30 18:37:04 +0300

    unnecessary item deleted

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -12,28 +12,15 @@ export class NaviComponent implements OnInit {
 
   canAddProduct: boolean=false;
   productAdd:string="productAdd"
-  ulogin = false;
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
     this.authService.getUserData();
-    this.islogin();
     this.authService.userData.operationClaims.forEach(element => {
       if (element.name == "productAdd") {
         this.canAddProduct = true
       }
     });
-  }
-  islogin() {
-    if (this.authService.loggedIn()) {
-      this.ulogin = true;
-    }
-  }
-  refreshLocalStorage() {
-    if (this.authService.loggedIn() != true) {
-      window.localStorage.clear();
-      window.location.reload()
-    }
   }
 
 }
